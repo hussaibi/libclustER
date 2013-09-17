8cb1fa26ad2a73677c551c61f2189fe51afb063b
hussaibi <b.hussain@utoronto.ca>
2013-09-17 06:52:06 -0400

    Removed Unnecessary source files
    Self explanatory.
    
    Note: 'inline' is used to speed up measure functions with C, while 'compiler' is used to speed up R code in general.

--- src/libcluster.R
+++ src/libcluster.R
@@ -1,9 +1,7 @@
 library("igraph");
 library("Matrix");
-#library("multicore");
 library("inline");
 library("compiler");
-library("rgp");
 source("load.R", echo=FALSE);
 source("partitioning.R", echo=FALSE);
 source("center.R", echo=FALSE);
@@ -15,8 +13,6 @@ source("correlation.R", echo=FALSE);
 source("mincut.R", echo=FALSE);
 source("articulation.R", echo=FALSE);
 source("markov.R", echo=FALSE);
-source("ensemble.R", echo=FALSE);
-source("test.R", echo=FALSE);
 
 source("sparse.apcluster/AllClasses.R", echo=FALSE);
 source("sparse.apcluster/cutree-methods.R", echo=FALSE);
@@ -24,7 +20,5 @@ source("sparse.apcluster/labels-methods.R", echo=FALSE);
 source("sparse.apcluster/show-methods.R", echo=FALSE);
 source("sparse.apcluster/apcluster.R", echo=FALSE);
 source("affinity.R", echo=FALSE);
-
-source("GPAugmentation.R", echo=FALSE);
 
 source("measures.R", echo=FALSE);
